5a47ba96ef138ab379bbbb07a5d43c8fe36668e8
vykot09 <55079449+vykot09@users.noreply.github.com>
2019-11-11 09:43:19 +0100

    Update Program.cs

--- ElsoValodiCsharpProjekt/ElsoValodiCsharpProjekt/Program.cs
+++ ElsoValodiCsharpProjekt/ElsoValodiCsharpProjekt/Program.cs
@@ -4,12 +4,141 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElsoValodiCsharpProjekt
+namespace ElsoValodiCharpProjekt
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /* 1. Feladat: 
+            string kiir = "Hello world!";
+            Console.WriteLine(kiir);*/
+
+            /* 2. Feladat:
+            Console.Write("Add meg a neved: ");
+            string nev = Console.ReadLine();
+            Console.WriteLine($"Üdvözöllek {nev}!");*/
+
+            /* 3. Feladat: 
+            Console.Write("Adj meg egy számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+            int szam = Convert.ToInt32(Console.ReadLine());
+            double szam = Double.Parse(Console.ReadLine());
+            double szam = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"A szám: {szam} kétszerese: {2 * szam}");*/
+
+            /* 4. feladat:
+            Console.Write("Adj meg egy számot: ");
+            int szamA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adj meg egy másik számot: ");
+            int szamB = Int32.Parse(Console.ReadLine());
+
+            int osszeg = szamA + szamB;
+            int kulonbseg = szamA - szamB;
+            int szorzat = szamA * szamA;
+            double hanyados = (double)szamA / szamB;
+
+            Console.WriteLine($"A két szám: {szamA} és {szamB}: ");
+            Console.WriteLine($"Összege: {osszeg}");
+            Console.WriteLine($"Különbsége: {kulonbseg}");
+            Console.WriteLine($"Szorzata: {szorzat}");
+            Console.WriteLine($"Hányadosa: {hanyados}");*/
+
+            /* 5. Feladat:
+            Console.Write("Adj meg egy számot: ");
+            int szamA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adj meg egy másik számot: ");
+            int szamB = Int32.Parse(Console.ReadLine());
+
+            if (szamA > szamB)
+            {
+                Console.WriteLine($"A nagyobbik szám: {szamA}");
+            }
+            else if (szamB > szamA)
+            {
+                Console.WriteLine($"A nagyobbik szám: {szamB}");
+            }
+            else
+            {
+                Console.WriteLine("A két szám egyenlő!");
+            }
+
+            if (szamA >= szamB)
+            {
+                Console.WriteLine($"A nagyobbik szám: {szamA}");
+            }
+            else
+            {
+                Console.WriteLine($"A nagyobbik szám: {szamB}");
+            }*/
+
+            /* 6. Feladat: 
+            Console.Write("Adj meg egy számot: ");
+            int szamA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adj meg egy másik számot: ");
+            int szamB = Int32.Parse(Console.ReadLine());
+            Console.Write("Adj meg egy harmadik számot: ");
+            int szamC = Convert.ToInt32(Console.ReadLine());
+
+            if (szamA <= szamB)
+            {
+                if (szamA <= szamC)
+                {
+                    Console.WriteLine($"A legkisebb szám: {szamA}");
+                }
+                else
+                {
+                    Console.WriteLine($"A legkisebb szám: {szamC}");
+                }
+            }
+            else
+            {
+                if (szamB <= szamC)
+                {
+                    Console.WriteLine($"A legkisebb szám: {szamB}");
+                }
+                else
+                {
+                    Console.WriteLine($"A legkisebb szám: {szamC}");
+                }
+            }*/
+
+            /* 7. Feladat:
+            Console.Write("Adj meg egy számot: ");
+            int szamA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adj meg egy másik számot: ");
+            int szamB = Int32.Parse(Console.ReadLine());
+            Console.Write("Adj meg egy harmadik számot: ");
+            int szamC = Convert.ToInt32(Console.ReadLine());
+
+            if (szamA + szamB > szamC && szamA + szamC > szamB && szamB + szamC > szamA)
+            {
+                Console.WriteLine($"A három számból: {szamA}, {szamB} és {szamC} háromszög szerkeszthető.");
+            }
+            else
+            {
+                Console.WriteLine($"A három számból: {szamA}, {szamB} és {szamC} háromszög nem szerkeszthető.");
+            }*/
+
+            /* 8. Feladat:
+            Console.Write("Adj meg egy számot: ");
+            int szamA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adj meg egy másik számot: ");
+            int szamB = Int32.Parse(Console.ReadLine());
+
+            if (szamA >= 0 && szamB >= 0)
+            {
+                double szamtani = ((double)szamA + szamB) / 2;
+                double mertani = Math.Sqrt(szamA * szamB);
+                Console.WriteLine($"A két szám: {szamA} és {szamB} számtani közepe: {szamtani}");
+                Console.WriteLine($"A két szám: {szamA} és {szamB} mértani közepe: {mertani}");
+            }
+            else
+            {
+                Console.WriteLine("A feladat nem megoldható!");
+            }*/
+
+            Console.ReadKey(true);
         }
     }
 }
