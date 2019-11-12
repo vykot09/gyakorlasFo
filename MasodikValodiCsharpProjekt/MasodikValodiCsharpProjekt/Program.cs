ba713457f2bd98bb95bff65d407d87fea8d77960
vykot09 <viktorbajari99@gmai.com>
2019-11-12 15:08:31 +0100

    ismetcommit

--- MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
+++ MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
@@ -10,6 +10,80 @@ namespace MasodikValodiCsharpProjekt
     {
         static void Main(string[] args)
         {
-        }
+            /* Console.WriteLine($"1 - 100 ig azok a számok melyek valódi osztóinak összege osztható héttl");
+             for (int i = 0; i < 101; i++)
+             {
+                 if (osszeghettel(i))
+                 {
+                     Console.WriteLine($" {szam} valódi osztóinak száma osztható 7*el ");
+                 }
+
+             }*/
+
+
+            /*int szam = 0
+                while (szam) {
+                Console.WriteLine($"Kérek egy egész sámot");
+                szam = Convert.ToInt32(Console.ReadLine)
+                    }
+            if (osszegettel(szam)
+            {
+                Console.WriteLine($" {szam} valódi osztóinak száma osztható 7*el ");
+            }
+            Console.WriteLine($"{szam} valódi osztóinak a száma nem oszthaó ");
+        } */
+
+            Console.WriteLine("2 től 100 ig primszamokat: ");
+
+            for (int i = 2; i < 101; i++)
+            {
+                if (PrimszamE(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
+
+        Console.ReadKey(true);
+    }
+        //gyors megoldas
+        static bool PrimszamE(int szam)
+        {
+            bool primE = true;
+            int hatar = (int)Math.Floor(Math.Sqrt(szam)) + 1;
+
+            for (int i = 0; i < hatar; i++)
+            {
+                if (szam % i == 0)
+                {
+                    primE = false;
+                    break;
+                }
+            }
+        } 
+
+
+
+       /* static bool osszeghettel(int szam);
+        int osszeg = 0;
+        bool hettel = true;
+
+        for (int i = 1; i < szam; i++)
+			{
+            if (szam % 1 == 0)
+	           { 
+                  osszeg == 1;
+
+	           }
+            }  
+            if (osszeg % 7(=0))
+	{
+              hettel = false;
+	}
+
+    */
+        
     }
 }
