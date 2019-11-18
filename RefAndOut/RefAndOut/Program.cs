7ecfc5010008337d198b02df736da9ec54905c65
vykot09 <viktorbajari99@gmai.com>
2019-11-18 09:48:09 +0100

    elsoprogramozas

---
+++ RefAndOut/RefAndOut/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*   Console.Write("Kérem az első számot: ");
+            int elso = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem a második számot: ");
+            int masodik = Int32.Parse(Console.ReadLine());
+
+            string nev = Console.ReadLine();
+            Console.WriteLine($"A két szám összege: {Osszead(ref elso, ref masodik)}");
+            Console.WriteLine($"A két szám: {elso}, {masodik}");
+            Console.WriteLine($"A név : {nev}");
+
+            Console.ReadKey(true);
+
+            //letrehozom a visszatero érteket és iniicializalom
+
+            int osszeg = 0;
+
+            osszeg = a + b;
+
+            return osszeg;
+
+            int osszeg = 0;
+            return osszeg = a + b;*/
+        }
+    }
+}
